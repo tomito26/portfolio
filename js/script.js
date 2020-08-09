e83391b9ef834934fc65a99578841371a2516991
tomito26 <tommybwah@gmail.com>
2020-08-09 17:18:27 +0300

    changed the landing page of the home and contact html form and correct afew errors on the contact form

--- js/script.js
+++ js/script.js
@@ -4,11 +4,13 @@ $(document).ready(function(){
         let name  =  $('input#name').val()
         let email = $('input#email').val()
         let message = $("textarea#message").val()
-        // alert(message)
+      
+        alert('Hello, ' + name + ' ,we have received your message, thank you for reaching out')
+      
         
-        alert(name + ' we have received your message ,thank you for reaching out')
         
         
 
     })
 })
+// alert('')
